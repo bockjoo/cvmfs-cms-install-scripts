f9e3aefed7779bd635ed50a1653b164619f8d1e2
bockjoo <bockjoo@phys.ufl.edu>
2018-02-28 10:47:54 -0500

    tex update

--- copy_afs_tex.sh
+++ copy_afs_tex.sh
@@ -2,6 +2,12 @@
 cvmfs_server transaction
 mkdir   /cvmfs/cms.cern.ch/external
 rsync -arzuvp --delete /afs/cern.ch/cms/external/tex /cvmfs/cms.cern.ch/external
+touch /cvmfs/cms.cern.ch/external/.README.WWWWHW
+touch /cvmfs/cms.cern.ch/external/.cvmfscatalog
+touch /cvmfs/cms.cern.ch/external/text/.cvmfscatalog
+touch /cvmfs/cms.cern.ch/external/tex/.cvmfscatalog
+vi /cvmfs/cms.cern.ch/.cvmfsdirtab 
+grep -q '^/external/\*' /cvmfs/cms.cern.ch/.cvmfsdirtab || echo '/external/*' >> /cvmfs/cms.cern.ch/.cvmfsdirtab /cvmfs/cms.cern.ch/.cvmfsdirtab || echo '/external/tex' >> /cvmfs/cms.cern.ch/.cvmfsdirtab
 cvmfs_server publish
 
 exit 0
